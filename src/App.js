4380f6f8e2a5427e2ce76f4a1372a100054dee2f
GarrixA <aphrogarrix10@gmail.com>
2023-10-05 21:02:56 +0200

    My commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Modal from 'react-modal'
-import { useState } from 'react';
-import Form from './components/Form';
-import Funk from './components/Funk';
-Modal.setAppElement('#root');
+import Inv from './components/Inv';
+
 
 
 function App() {
-  const [popForm, setPopForm ] = useState(false)
+  
   return (
-    <div>
-      <button onClick={() => setPopForm(true)}>open modal</button>
-      <Modal isOpen={popForm}>
-        {/* <Form /> */}
-        <Funk />
-        <div>
-          <button className='add' onClick={(addition) => setPopForm(true)}>Add</button>
-          <button onClick={() => setPopForm(false)}>Close</button>
-        </div>
-      </Modal>
-    </div>
+    <Inv />
   );
 }
 
---
+++ src/components/Inv.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+Modal.setAppElement('#root');
+
+function Inv (){
+    const [popForm, setPopForm ] = useState(false);
+    return (
+        <div className="main-inv">
+            <div className="inv-text">
+                <h1>Invoices</h1>
+                <p>There are 7 total invoices</p>
+            </div>
+            <div className="btns">
+                <button className="btn1" onClick={() => setPopForm(true)}> Add invoice </button>
+                <Modal  isOpen={popForm}>              
+                {/* <Form /> */}
+                    
+                    <div>
+                    <button onClick={() => setPopForm(false)}> Close</button>
+                    </div>
+                </Modal>
+            </div>
+        </div>
+    )
+}
+
+export default Inv
